46f0db1df834306bd10ab8a6129dd3ce040c46fc
matforest <mat@forest.id.au>
2019-05-14 12:42:21 +0930

    Initial working version of express

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 console.log('Running calendar stuff');
 
+var express = require('express');
+var app = express();
+
 const https = require('https');
 const filter = require('./lib/filter')
 
-let holidayUrl = "https://calendar.google.com/calendar/ical/en.australian%23holiday%40group.v.calendar.google.com/public/basic.ics";
+app.get("/calendar/saholidays", sendSACalendar);
+function sendSACalendar(req, res) {
 
-https.get(holidayUrl, (incomingMessage) => {
-    console.log('Get completed');
+    let holidayUrl = "https://calendar.google.com/calendar/ical/en.australian%23holiday%40group.v.calendar.google.com/public/basic.ics";
+    https.get(holidayUrl, (incomingMessage) => {
 
-    incomingMessage.setEncoding('UTF-8');
-    let rawData = '';
-    incomingMessage.on('data', (chunk) => { rawData += chunk; });
-    incomingMessage.on('end', () => {
-        try {
-            console.log(`Message ended`);
-            filter.filterCalendar(rawData);
-        } catch (e) {
-            console.error('Error!', e.message, e);
-        }
+        incomingMessage.setEncoding('UTF-8');
+        let rawData = '';
+        incomingMessage.on('data', (chunk) => {
+            rawData += chunk;
+        });
+        incomingMessage.on('end', () => {
+            try {
+                let saCalendar = filter.filterCalendar(rawData);
+                res.send(saCalendar);
+            } catch (e) {
+                console.error('Error!', e.message, e);
+            }
+        });
     });
+}
+
+
+// TESTING FIXME
+app.get('/', (req, res) => {
+    res.send('ok');
+});
+
+let port = process.argv[2];
+let httpServer = app.listen(port, function () {
+    console.log("Listening on port " + port);
+});
+
+process.on('exit', function () {
+    console.log('About to close');
+    httpServer.close();
 });
 
